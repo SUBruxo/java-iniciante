fdeebe394340ea5ade22f100734770116214bd60
SUBruxo <148587400+SUBruxo@users.noreply.github.com>
2026-01-25 23:41:03 -0300

    Create VariaveisBasicas.java
    Exercício com variáveis básicas em Java

---
+++ fundamentos/VariaveisBasicas.java
@@ -0,0 +1,12 @@
+public class VariaveisBasicas {
+    public static void main(String[] args) {
+
+        String nome = "João";
+        int idade = 18;
+        double altura = 1.75;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+    }
+}
